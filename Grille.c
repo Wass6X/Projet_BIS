ab1c27d40656ee4f81f8e5788843b2d1239b7a1c
pkno <youcef@youcef-ubuntu-univ.youcef-ubuntu-univ.virtualbox.org>
2024-04-30 16:23:06 +0200

    v6

--- Grille.c
+++ Grille.c
@@ -4,10 +4,20 @@
 #include "Grille.h"
 
 struct Grille * Grille_allouer(int n, int m){
+        
+        int i;
+        
         struct Grille *g = malloc(sizeof(struct Grille));
+        
         g->n = n;
         g->m = m;
-        g->tab = malloc(9*sizeof(char *));
+
+        g->tab = malloc(8 * sizeof(char *));
+	
+	for (i=0; i<m; i++) {
+		tab[i] = malloc(8 * sizeof(char));		
+	}	
+
 }
 
 struct Grille vider 
