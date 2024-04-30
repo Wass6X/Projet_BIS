49e7643f81c7439b38a888e7ed4eb37cc9c2095b
pkno <youcef@youcef-ubuntu-univ.youcef-ubuntu-univ.virtualbox.org>
2024-04-30 15:29:23 +0200

    v7

--- Grille.h
+++ Grille.h
@@ -2,15 +2,15 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define MAX 200
 
 struct Grille {
-  int n;
-  int m;
-  char mat[MAX][MAX];
-  int long_fruit;
-  int larg_fruit;
+ 	int n;
+ 	int m;
+	char ** mat;
+	int long_fruit;
+	int larg_fruit;
 };
+
 
 struct Grille *Grille_allouer(int n, int m);
 char Grille_vider();
