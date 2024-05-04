5a1591dc5b6240350782039651d78dbad21ecd79
Wass6X <wissameddineb@gmail.com>
2024-05-04 15:20:03 +0200

    ajout de parentheses

--- Grille.c
+++ Grille.c
@@ -87,7 +87,7 @@ void Grille_redessiner(struct Grille *g){
 			if(i==-1){
 				printf("\033[42m▄");
 			}else if(i==g->n){
-				printf("\033[42m▀";
+				printf("\033[42m▀");
 			}else if (j == -1 || j == g->m) {
                 		printf("\033[42m ");
             		}else {
