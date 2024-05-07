a0a35062fb29aea4d0a37317f917134083c9df08
Wass6X <wissameddineb@gmail.com>
2024-05-07 11:26:01 +0200

    Update Serpent.c

--- Serpent.c
+++ Serpent.c
@@ -6,12 +6,12 @@ struct Serpent * creer_serpent(int n, int m) {
 
 	struct Serpent * serp = malloc(sizeof(struct Serpent)); 
     
-	serp->cordx = 10;
-	serp->cordy = 10;
+	serp->cordx = (n/2);
+	serp->cordy = (m/2);
     
 	serp->chaine = creer_liste();
 	
-	ajout_debut_liste(serp->chaine, creer_section(2, 42));
+	ajout_debut_liste(serp->chaine, creer_section(1, 42));
     
 	return serp;
 }
