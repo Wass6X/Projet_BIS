869b356126f3cf83ad11409c9cf2b78efba4cf68
Wass6X <wissameddineb@gmail.com>
2024-05-08 19:32:38 +0200

    maj

--- Grille.c
+++ Grille.c
@@ -158,14 +158,10 @@ void Grille_remplir_couleur(struct Grille * g, int x, int y, int couleur) {
 // simplification de grille remplir pour une seule case
 void Grille_remplir_serp(struct Grille * g, struct Serpent * serp) {
     
-    struct Section * s;
-    s = extraire_section(serp->chaine);
-
-
     if (serp == NULL || g == NULL) return;
 
     if (serp->cordx >= 0 && serp->cordx < g->n && serp->cordy >= 0 && serp->cordy < g->m) {
-        Grille_remplir_couleur(g, serp->cordx, serp->cordy, s->couleur);
+        Grille_remplir_couleur(g, serp->cordx, serp->cordy, 45);
     }
 
 }
